ea6c002f8bd2e201cdc7f902d4aed45d487d5858
Abhinav <145043961+AbhinavInGit@users.noreply.github.com>
2025-04-17 09:33:32 +0530

    Add files via upload

---
+++ 01_intro.py
@@ -0,0 +1,5 @@
+#loops 
+#range(6) = print till range 6 
+
+for i in range(5, 20, 3): #this will start from 5 and increment by3
+    print(i , end =" ")
---
+++ 02_inputRange.py
@@ -0,0 +1,4 @@
+n = int(input("enter the number :"))
+print("Table of : ", n)
+for i in range (1,11):
+    print(n , "X", i , "=", n*i)
