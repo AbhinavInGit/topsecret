06a512a7996a1169b0b91870154225fa363bbed0
Abhinav <145043961+AbhinavInGit@users.noreply.github.com>
2025-04-23 13:51:55 +0530

    Create numberPyramid.py

---
+++ numberPyramid.py
@@ -0,0 +1,11 @@
+
+for i in range(10):
+    for j in range (10-i):
+        print(end=" ")
+    for j in range(1,i):
+        print(j, end = "")
+    for j in range(1,i-1):
+        print(j, end = "")
+        
+        
+    print()
